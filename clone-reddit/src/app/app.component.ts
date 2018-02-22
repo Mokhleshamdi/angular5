dc3989007c4edab288b4e7439b0ae241a9b0583f
mokhles <mokhleshmd1@gmail.com>
2018-02-22 13:06:58 +0100

    1st angular application

---
+++ clone-reddit/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {Article} from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles: Article[] ;
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+  constructor() {
+    this.articles = [
+      new Article('java', 'www.java.com', 7),
+      new Article('Android', 'www.android.com', 10),
+      new Article('Nodejs', 'www.nodeJs.com', 4)
+    ];
+
+  }
+}
